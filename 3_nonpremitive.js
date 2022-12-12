f20331e75b50c622d7ec2dc17f5b07c4bc831cfa
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2022-12-12 20:24:08 +0530

    Add files via upload

---
+++ 3_nonpremitive.js
@@ -0,0 +1,20 @@
+//in previous tutrorial we have learned what are the premitive data type is
+// ans there are 7 of them
+
+// herw we are seeing other data type than primitives which are objects
+
+//objects are beautiful in javascript
+
+let myinfo={
+    "name":"dhruvin",
+    "city":"surat",
+    "country":"india",
+    "mobile": 365234543,
+    "marital status":undefined,
+}
+
+//we can acess each key:value pair by the .operator
+ 
+console.log(myinfo.mobile);
+//and all the rules of naming of varible will apply to on name giving to the keys
+// try by doing yourself
