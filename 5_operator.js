7493be5c57cb0b4e23282fa64213d1360b48d37e
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2022-12-15 22:46:27 +0530

    Add files via upload

---
+++ 5_operator.js
@@ -0,0 +1,21 @@
+
+
+let a=10;
+console.log(a++);// prints 10
+console.log(++a);//prints 11
+
+console.log(a--);//prints 10
+console.log(--a);//prints 9
+
+//haa par sab line ek saath mat run karna, ek ek karke karna
+
+//so, conclusion of the arithmatic operator is 
+//++a incremernt and print
+//a++ print and increment
+
+let b=7;
+let c="6";
+// console.log(b==c);    //ye to hamesha true hi return karega
+// console.log(b!=c);
+// console.log(b===c);   //par ye sirf jab "absolute value" ke equal hone pe hi true return karega
+console.log(b!==c);
